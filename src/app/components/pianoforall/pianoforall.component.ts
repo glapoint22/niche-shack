851462f226aabd0e8e2e53f8d7d2776b9f5dd03a
Gabe LaPoint <glapoint22@gmail.com>
2024-01-19 01:32:50 -0500

    working with pianoforall

--- src/app/components/pianoforall/pianoforall.component.ts
+++ src/app/components/pianoforall/pianoforall.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'ns-pianoforall',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage],
   templateUrl: './pianoforall.component.html',
   styleUrls: [
     './styles/pianoforall.component.scss',
     './styles/pianoforall.component.video-section.scss',
     './styles/pianoforall.component.product-section.scss',
-    './styles/pianoforall.component.works-section.scss'
+    './styles/pianoforall.component.works-section.scss',
+    './styles/pianoforall.component.ebooks-section.scss',
+    './styles/pianoforall.component.sample-video-section.scss',
+    './styles/pianoforall.component.perks-section.scss',
+    './styles/pianoforall.component.end-section.scss'
   ]
 })
 export class PianoforallComponent {
